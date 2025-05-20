15ed363fae73fdc19b0ae7e87db9c6c3c59ccff9
Maruf <hossain.maruf186@gmail.com>
2025-05-19 22:22:04 -0400

    generated docs

--- lib/models/user.ts
+++ lib/models/user.ts
@@ -1,5 +1,14 @@
 import {Schema, model, models} from "mongoose";
 
+/**
+ * Defines the schema for the User model.
+ *
+ * @property {string} email - The user's email address. Must be unique and is required.
+ * @property {string} username - The user's username. Must be unique and is required.
+ * @remarks
+ * Timestamps for creation and updates are automatically managed.
+ */
+
 const UserSchema = new Schema({
     email: {type: "string", required: true, unique: true},
     username: {type: "string", required: true, unique: true}
