c46555a49be7efec5d920783ecbce40d26110d84
Maruf <hossain.maruf186@gmail.com>
2025-05-23 19:13:48 -0400

    done with api

--- app/api/(dashboard)/blogs/route.ts
+++ app/api/(dashboard)/blogs/route.ts
@@ -10,6 +10,11 @@ export const GET = async (request: Request) => {
         const { searchParams } = new URL(request.url);
         const userId = searchParams.get("userId");
         const categoryId = searchParams.get("categoryId");
+        const searchKeywords = searchParams.get("Keywords") as string;
+        const startDate = searchParams.get("startDate");
+        const endDate = searchParams.get("endDate");
+        const page: any = parseInt(searchParams.get("page") || "1");;
+        const limit: any = parseInt(searchParams.get("limit") || "10");
 
         if (!userId || !Types.ObjectId.isValid(userId)) {
             return new NextResponse(JSON.stringify({ message: "Invalid User id" }), {
@@ -41,7 +46,33 @@ export const GET = async (request: Request) => {
             user: new Types.ObjectId(userId),
             category: new Types.ObjectId(categoryId),   
         };
-        const blogs = await Blog.find(filter);
+
+        if (searchKeywords) {
+            filter.$or = [
+                { title: { $regex: searchKeywords, $options: "i" } },
+                { desciption: { $regex: searchKeywords, $options: "i" } },
+            ];
+        }
+
+        if (startDate && endDate) {
+            filter.createdAt = {
+                $gte: new Date(startDate),
+                $lte: new Date(endDate),
+            };
+        } else if (startDate) {
+            filter.createdAt = {
+                $gte: new Date(startDate),
+            };
+        } else if (endDate) {
+            filter.createdAt = {
+                $lte: new Date(endDate),
+            };
+        }
+        
+        const skip = (page - 1) * limit;
+
+        const blogs = await Blog.find(filter).sort({ createdAt: "asc"}).skip(skip).limit(limit);
+
         return new NextResponse(JSON.stringify(blogs), {
             status: 200,
         });
